9c723ea7066ddadb80e3378aa339dc338cdd6c61
MrPrograMax <grigorievma9461@gmail.com>
2024-06-24 03:36:24 +0700

    init project

---
+++ pkg/service/service.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"ims-product-api/pkg/repository"
+)
+
+type Services struct {
+	//TODO
+}
+
+func NewService(repos *repository.Repository) *Services {
+	return &Services{
+		//TODO
+	}
+}
